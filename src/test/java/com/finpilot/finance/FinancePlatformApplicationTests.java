e948a4e950672cae6eda6161d704e580649a2f81
Hatim Husain <hatim.husain24@gmail.com>
2026-05-30 20:27:00 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/finpilot/finance/FinancePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finpilot.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
